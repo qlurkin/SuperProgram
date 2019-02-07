d7df55b0aa440618915dd071e04f2bc6e772f6f5
Quentin Lurkin <qlurkin@gmail.com>
2019-02-07 10:34:32 +0100

    Make usefull thing

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello prout prout prout prout caca")
+import json
+import sys
+
+with open('db.json') as file:
+	db = json.load(file)
+
+if len(sys.argv) < 2:
+	print("Usage: main.py <matricule>")
+	sys.exit()
+
+matricule = sys.argv[1]
+
+indexedDB = {}
+
+for student in db:
+	indexedDB[student['matetu']] = student
+
+
+print(indexedDB[matricule]['npetu'])
+
